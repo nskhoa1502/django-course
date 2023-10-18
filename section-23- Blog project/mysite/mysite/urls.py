0c3f9093443234fd4a3da429b185f825f659b1e0
nskhoa1502 <nskhoa1502@gmail.com>
2023-10-18 23:40:37 +0700

    login page

--- section-23- Blog project/mysite/mysite/urls.py
+++ section-23- Blog project/mysite/mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path,include
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'',include('blog.urls')),
+    re_path(r'^accounts/login/$',views.LoginView.as_view(),name='login'),
+    re_path(r'^accounts/logout/$',views.LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}), # next_page redirects to the homepage
 ]
