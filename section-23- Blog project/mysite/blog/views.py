f366fb324d5cfeb7d2772901a14aef86667f8db2
nskhoa1502 <nskhoa1502@gmail.com>
2023-10-18 21:35:01 +0700

    add-CRUD-Comment

--- section-23- Blog project/mysite/blog/views.py
+++ section-23- Blog project/mysite/blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect 
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-# from django.contrib.auth.decorators import login_required - ONLY FUNCTION VIEW
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required # for function based views
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 # Create your views here.
@@ -52,3 +52,57 @@ class DraftListView(LoginRequiredMixin,ListView):
         # __isnull is a Django convention for checking if something is null
         # If the published_date is null, then we know that the post is a draft
         return Post.objects.filter(published_date__isnull=True).order_by('create_date')
+    
+
+# ===============================
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish() # Call the publish method in the Post model
+    return redirect('post_detail',pk=pk) # Redirect to the post_detail page of the post that was published based on the primary key
+
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST) # Get the form data
+
+        if form.is_valid():
+            comment = form.save(commit=False) # commit=False means don't save to database, just get the form data and save it in memory
+            comment.post = post # Assign the comment to the post, because post is a foreign key in the Comment model
+            comment.save() # Save to database
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm() # If the request is not a POST request, then just create a blank form
+    
+    return render(request,'blog/comment_form.html',{'form':form}) # Pass the form to the template
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve() # Call the approve method in the Comment model
+    return redirect('post_detail',pk=comment.post.pk) # Redirect to the post_detail page of the post that the comment belongs to based on the primary key
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk # Get the primary key of the post that the comment belongs to
+    comment.delete() # Delete the comment
+    return redirect('post_detail',pk=post_pk) # Redirect to the post_detail page of the post that the comment belongs to based on the primary key
+
+@login_required
+def comment_edit(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk # Get the primary key of the post that the comment belongs to
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST,instance=comment) # We pass in the instance of the comment so that the form knows which comment to update
+        if form.is_valid():
+            comment = form.save()
+            return redirect('post_detail',pk=post_pk)
+    else:
+        form = CommentForm(instance=comment) # We pass in the instance of the comment so that the form knows which comment to update
+    return render(request,'blog/comment_form.html',{'form':form})
+
