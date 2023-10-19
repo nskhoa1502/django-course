5e6c13e6a62dc08e7547b31acc9836ee4626c5d9
nskhoa1502 <nskhoa1502@gmail.com>
2023-10-20 06:21:47 +0700

    setup-models-for-groups-and-posts-apps

--- section-24-social-media-clone/simplesocial/groups/models.py
+++ section-24-social-media-clone/simplesocial/groups/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+
 
 # Create your models here.
+import misaka
+from django.contrib.auth import get_user_model
+User = get_user_model() # This is a way to get the current user model that is active in this project
+
+from django import template
+register = template.Library() # This is a way to register custom template tags
+
+class Group(models.model):
+    name = models.CharField(max_length=255,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    description = models.TextField(blank=True,default='')
+    description_html = models.TextField(editable=False,default='',blank=True)
+    members = models.ManyToManyField(User,through='GroupMember') # This is a way to create a many to many relationship between the Group model and the User model
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description) # This is a way to convert the description to html
+        super().save(*args,**kwargs)  # super is used to call the save method of the parent class, in this case, the parent class is models.model
+
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+    
+    class Meta:
+        ordering = ['name'] # This is to make sure that the groups are ordered by name
+    
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group,related_name='memberships',on_delete=models.CASCADE) 
+    user = models.ForeignKey(User,related_name='user_groups',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        unique_together = ('group','user') # This is to make sure that a user can only be a member of a group once
--- section-24-social-media-clone/simplesocial/posts/models.py
+++ section-24-social-media-clone/simplesocial/posts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+
+import misaka
+from groups.models import Group
 
 # Create your models here.
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Post(models.Model):
+    user = models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE) # This is a way to create a many to many relationship between the Post model and the User model
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False) # This is to make sure that the message is not editable
+    group = models.ForeignKey(Group,related_name='posts',null=True,blank=True,on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.message
+    
+    def save(self,*args,**kwargs):
+        self.message_html = misaka.html(self.message) # This is a way to convert the message to html
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts:single',kwargs={'username':self.user.username,'pk':self.pk}) # This is to make sure that the user is redirected to the post detail page after creating a post
+    
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user','message'] # This is to make sure that a user can only post a message once
